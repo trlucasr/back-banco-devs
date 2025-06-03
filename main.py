40117e82c0fda3b6ac5d3d9a1afe5ea01a9936c9
Lucas Ribeiro <79470623+trlucasr@users.noreply.github.com>
2025-06-03 18:19:03 -0300

    .

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from typing import Union
 from fastapi import FastAPI
 from clientes import router as clientes_router
 
-app = FastAPI()
+app = FastAPI( 
+    title="Banco Devs",
+    description="Banco Financeiro que realiza transações bancárias entre contas cadastradas",
+    version="1.0.0")
 
 @app.get("/")
 def welcome_banco_devs():
