8d3256a86e6a77b7d326fe5e8d6fe562ab3fb83f
Lucas Ribeiro <79470623+trlucasr@users.noreply.github.com>
2025-07-22 20:26:44 -0300

    .

--- contas.py
+++ contas.py
@@ -6,7 +6,7 @@ from db import Database
 router = APIRouter()
 db = Database()
 
-class Conta(BaseModel):
+class ContaPost(BaseModel):
     id: int = None
     id_cliente: int = None
     banco: str
@@ -14,26 +14,34 @@ class Conta(BaseModel):
     cc: str
     status: str = None
 
+class ContaPut(BaseModel):
+    id: int 
+    banco: str = None
+    agencia: str = None
+    cc: str = None
+    status: str = None
+
 
 def validaRequest(body, endpoint):
 
     if endpoint == "create_conta":
-        if body.id_cliente is not None and body.id_cliente > 0:
-            body.id_conta = 0
+        if body.id_cliente is None or body.id_cliente == 0:
+            body.id_cliente = 0
 
         if body.id is not None and body.id > 0:
             body.id = 0
             return {"status": 4001, "mensagem": "id não deve ser enviado na requisição."}
 
     if endpoint == "alterar_conta":
-        pass
+        if body["id"] is None or body["id"] == 0:
+            body["id"] = 0
+            return {"status": 4002, "mensagem": "id é obrigatório e não pode ser zerado para executar essa requisição."}
 
     return {"status": 2000, "mensagem": "OK"}
 
     
-
 @router.post("/conta/", tags=["Contas"])
-async def criar_conta(body: Conta):
+async def criar_conta(body: ContaPost):
 
     """
     Cria uma nova conta.
@@ -56,7 +64,7 @@ async def criar_conta(body: Conta):
     query = """INSERT INTO contas
     (id_cliente, banco, agencia, cc, status)
     VALUES ( {},'{}', '{}', '{}', '{}')""".format(
-        0,
+        body.id_cliente,
         body.banco,
         body.agencia,
         body.cc,
@@ -100,7 +108,7 @@ async def deletar_conta(id: int):
     return db.query(query=f"DELETE FROM contas WHERE id = {id};", autoCommit=True)
 
 @router.put("/conta/", tags=["Contas"])
-async def atualizar_conta(body: Conta):
+async def atualizar_conta(body: ContaPut):
     
     """
     Atualiza uma conta existente.
@@ -111,13 +119,33 @@ async def atualizar_conta(body: Conta):
     Returns:
     - dict: Contendo o status da operação e uma mensagem de erro,
       caso haja algum problema na requisição.
-    """
+    """    
+
+    body = body.dict() 
 
     r_valid = validaRequest(body, "alterar_conta")
 
     print(r_valid)
 
     if r_valid["status"] > 4000:
-        return {"status": r_valid["status"], "mensagem": r_valid["mensagem"]} 
+        return {"status": r_valid["status"], "mensagem": r_valid["mensagem"]}
 
-    
+    body["status"] = "R"
+
+    query = """UPDATE contas SET """
+
+    if body.get("banco"):
+        query += "banco = '{}', ".format(body["banco"])
+    if body.get("agencia"):
+        query += "agencia = '{}', ".format(body["agencia"])
+    if body.get("cc"):
+        query += "cc = '{}', ".format(body["cc"])
+    if body.get("status"):
+        query += "status = '{}', ".format(body["status"])
+
+    query = query.rstrip(", ") + " WHERE id = {}".format(body["id"])
+
+    return db.query(
+        query=query,
+        autoCommit=True,
+    )
